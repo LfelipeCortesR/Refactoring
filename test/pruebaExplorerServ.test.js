0780238b05ab3c7b4b8bf6f5b970863a413e04ff
LfelipeCortesR <lfelipecr76@hotmail.com>
2022-04-27 16:31:39 -0500

    prueba services2

--- test/pruebaExplorerServ.test.js
+++ test/pruebaExplorerServ.test.js
@@ -3,7 +3,7 @@ describe("Esto es una suite de pruebas", () => {
         const Reader = require("./../lib/utils/Reader")
         const ExplorerService = require("./../lib/services/ExplorerService")
 
-        const explorers=Reader.readJsonFile("explorers.json") 
+        const explorers=Reader.readJsonFile("explorers.json")
         console.log(ExplorerService.filterByMission(explorers, "node"))
     });
   })
