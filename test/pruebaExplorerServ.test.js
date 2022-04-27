0626859e584be79558e18d550671d8f0febf3633
LfelipeCortesR <lfelipecr76@hotmail.com>
2022-04-27 16:24:51 -0500

    Prueba service

---
+++ test/pruebaExplorerServ.test.js
@@ -0,0 +1,9 @@
+describe("Esto es una suite de pruebas", () => {
+    test('Caso de prueba 1', () => {
+        const Reader = require("./../lib/utils/Reader")
+        const ExplorerService = require("./../lib/services/ExplorerService")
+
+        const explorers=Reader.readJsonFile("explorers.json") 
+        console.log(ExplorerService.filterByMission(explorers, "node"))
+    });
+  })
