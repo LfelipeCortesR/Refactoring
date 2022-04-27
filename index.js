d1b499b487274e64905fc2871bad31f5c481e96e
LfelipeCortesR <lfelipecr76@hotmail.com>
2022-04-27 15:21:33 -0500

    Agregando nuevos archivos

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const Reader=require("./../refactoring/lib/utils/Reader")
 
-const explorers=Reader.readJsonFile("explorers.json")
-console.log(explorers)
+// Clase anterior con la que obtenemos los explorers
+const Reader = require("./lib/utils/Reader");
+const explorers = Reader.readJsonFile("explorers.json");
+
+// Aplicación del ExplorerService sobre la lista de explorers
+ExplorerService.filterByMission(explorers, "node");
+ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+ExplorerService.getExplorersUsernamesByMission(explorers, "node");
