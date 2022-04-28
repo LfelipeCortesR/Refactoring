c42fa61e15760c6141c07a05c8e0bbc6a4c391e2
LfelipeCortesR <lfelipecr76@hotmail.com>
2022-04-28 15:49:38 -0500

    fallo apropocito

--- test/pruebaRead.test.js
+++ test/pruebaRead.test.js
@@ -2,7 +2,7 @@ describe("Esto es una suite de pruebas", () => {
     test('Caso de prueba 1', () => {
         const Reader = require("./../lib/utils/Reader")
 
-        const explorers=Reader.readJsonFile("explorers.json") 
+        const explorers=Reade.readJsonFile("explorers.json") 
         console.log(explorers)
     });
   })
