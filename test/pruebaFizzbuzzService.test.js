8679f278ca8736335f1ab8a6d4a20031e44c3a10
LfelipeCortesR <lfelipecr76@hotmail.com>
2022-04-28 14:07:28 -0500

    agregando archivos nuevos

---
+++ test/pruebaFizzbuzzService.test.js
@@ -0,0 +1,18 @@
+describe("Esto es una suite de pruebas", () => {
+    test('Caso de prueba 1', () => {
+        const FizzbuzzService = require("./../lib/services/FizzbuzzService")
+
+        const explorer1 = {name: "Explorer1", score: 1}
+        // {name: "Explorer1", score: 1, trick: 1} 
+        console.log(FizzbuzzService.applyValidationInExplorer(explorer1))
+
+        const explorer3 = {name: "Explorer3", score: 3}
+        FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+        const explorer5 = {name: "Explorer5", score: 5}
+        FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+        const explorer15 = {name: "Explorer15", score: 15}
+        FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+    });
+  })
