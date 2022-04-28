b2be1af9955c0ba48cb52281c12f23ae41c05feb
LfelipeCortesR <lfelipecr76@hotmail.com>
2022-04-28 14:16:55 -0500

    actualizacon de pruebas y mala exportacion

--- test/pruebaFizzbuzzService.test.js
+++ test/pruebaFizzbuzzService.test.js
@@ -7,12 +7,12 @@ describe("Esto es una suite de pruebas", () => {
         console.log(FizzbuzzService.applyValidationInExplorer(explorer1))
 
         const explorer3 = {name: "Explorer3", score: 3}
-        FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+        console.log(FizzbuzzService.applyValidationInExplorer(explorer3)) // {name: "Explorer3", score: 3, trick: "FIZZ"}
 
         const explorer5 = {name: "Explorer5", score: 5}
-        FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+        console.log(FizzbuzzService.applyValidationInExplorer(explorer5)) // {name: "Explorer5", score: 5, trick: "BUZZ"}
 
         const explorer15 = {name: "Explorer15", score: 15}
-        FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+        console.log(FizzbuzzService.applyValidationInExplorer(explorer15)) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
     });
   })
